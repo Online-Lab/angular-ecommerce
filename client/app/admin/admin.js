01c3b677b0acda20c99bfe1d0c5d57720b413a35
fsanano <memonanofsa@gmail.com>
2015-06-01 12:37:43 +0300

    start project

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('angularEcommerceApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
